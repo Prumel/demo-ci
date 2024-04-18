f67fc3baf1613d12407e86c34563d5cd2661009b
Ludi <melissaljf@orange.fr>
2024-04-18 14:55:43 +0200

    first commit

---
+++ src/main/java/com/formation/democi/DemoCiApplication.java
@@ -0,0 +1,13 @@
+package com.formation.democi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoCiApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiApplication.class, args);
+	}
+}
